6375fc80b98fd0e6d9c343cda9e3896aba9a197c
orceN2k <dominic.schuermann@bluewin.ch>
2015-03-14 16:33:26 +0100

    add exercise four

--- Aufgabe2/app/src/main/java/mobpro/aufgabe2/ViewsDemoActivity.java
+++ Aufgabe2/app/src/main/java/mobpro/aufgabe2/ViewsDemoActivity.java
@@ -1,10 +1,53 @@
 package mobpro.aufgabe2;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * Created by orceN on 14.03.2015.
  */
 public class ViewsDemoActivity  extends Activity{
 
+    private TextView topLabel;
+    private RatingBar ratingBar;
+    private SeekBar seekBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_views_demo);
+
+        topLabel = (TextView)findViewById(R.id.viewdemo_fluentlabel);
+
+        ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener( new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                topLabel.setText("Das aktuelle Rating ist: " + rating);
+            }
+        });
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                topLabel.setText("Der Progress liegt bei: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
 }
